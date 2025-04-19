5d7bd24529c36d4b1d773d3a91f4a864afcf7c79
Aaryankumar13084 <aaryankumar13084@gmail.com>
2025-04-19 03:23:34 +0000

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -427,6 +427,8 @@ bot.on("callback_query", async (ctx) => {
           Markup.button.callback("Level 1", "change_to_2"),
           Markup.button.callback("Level 2", "change_to_3"), 
           Markup.button.callback("Level 3", "change_to_4"), 
+         Markup.button.callback("Level 4", "change_to_5"), 
+         Markup.button.callback("Level 5", "change_to_6"), 
         ])
       );
     }
